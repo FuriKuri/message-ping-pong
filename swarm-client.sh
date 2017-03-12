25d96aaf43c87cc5f987c9505bbd2fc2077570da
Theo Pack Test <tf.pack@gmail.com>
2017-03-12 12:56:59 +0100

    Add swarm visualization

--- swarm-client.sh
+++ swarm-client.sh
@@ -4,3 +4,5 @@ docker service create --name game-client-ping --replicas 2 --network game-networ
 docker service create --name game-client-pong --replicas 2 --network game-network -e SPRING_PROFILES_ACTIVE=pong -e KAFKA_BOOTSTRAP_SERVERS=kafka:9092 furikuri/game-client
 docker service create --name game-controller --replicas 2 --network game-network -e KAFKA_BOOTSTRAP_SERVERS=kafka:9092 furikuri/game-controller
 docker service create --name game-proxy -p 8080:80 --mode global --network game-network furikuri/game-proxy
+
+docker service create --name=viz -p 8081:8080 --constraint=node.role==manager --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock manomarks/visualizer
