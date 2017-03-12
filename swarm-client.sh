7fe4433301bcedd2c5ece41c22f5881d14db3c65
Theo Pack Test <tf.pack@gmail.com>
2017-03-12 12:42:33 +0100

    Set proxy mode to global

--- swarm-client.sh
+++ swarm-client.sh
@@ -3,4 +3,4 @@
 docker service create --name game-client-ping --replicas 2 --network game-network -e SPRING_PROFILES_ACTIVE=ping -e KAFKA_BOOTSTRAP_SERVERS=kafka:9092 furikuri/game-client
 docker service create --name game-client-pong --replicas 2 --network game-network -e SPRING_PROFILES_ACTIVE=pong -e KAFKA_BOOTSTRAP_SERVERS=kafka:9092 furikuri/game-client
 docker service create --name game-controller --replicas 2 --network game-network -e KAFKA_BOOTSTRAP_SERVERS=kafka:9092 furikuri/game-controller
-docker service create --name game-proxy -p 8080:80 --network game-network furikuri/game-proxy
+docker service create --name game-proxy -p 8080:80 --mode global --network game-network furikuri/game-proxy
